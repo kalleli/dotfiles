b2f7db368067e7b7ebc700bc5ea0a1d63de14e18
Kalle Lindblom <kalle.lindblom@excillum.com>
2026-08-28 14:45:03 +0200

    Map ö/ä to bracket motions in nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local map = vim.keymap.set
+
+map({ "n", "x", "o" }, "ö", "[", { remap = true })
+map({ "n", "x", "o" }, "ä", "]", { remap = true })
+map({ "n", "x", "o" }, "Ö", "{")
+map({ "n", "x", "o" }, "Ä", "}")
