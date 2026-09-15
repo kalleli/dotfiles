01ecef0653965aff2302a8083f00f7a03e303115
Kalle Lindblom <kalle.lindblom@excillum.com>
2026-09-15 13:33:59 +0200

    Track diffview and git-conflict plugin specs

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+          disable_diagnostics = true,
+        },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/git-conflict.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  config = true,
+  opts = {
+    default_mappings = true,
+    default_commands = true,
+    disable_diagnostics = false,
+  },
+}
